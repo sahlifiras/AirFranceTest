1d78adb7f14ee5db0901fca34624df567919f4f7
firas26 <firas.sahli@aymax.fr>
2021-06-14 11:46:03 +0100

    initialize project

---
+++ src/main/java/com/airfrance/testfirassahli/TestfirassahliApplication.java
@@ -0,0 +1,13 @@
+package com.airfrance.testfirassahli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfirassahliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestfirassahliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/airfrance/testfirassahli/TestfirassahliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airfrance.testfirassahli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfirassahliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
